358f44a0df8b45e54641bd0c3f4d96e48228ff65
bkrumnow <benjamin.krumnow@th-koeln.de>
2019-07-19 11:13:59 +0200

    test_set added. Adjusted screenshot files

--- screenshot_bot.py
+++ screenshot_bot.py
@@ -10,9 +10,8 @@ import pdb
 #NUM_BROWSERS = 8
 #fileReader = csv.reader(open('./sites/bot_detectors.csv'), delimiter=',')
 
-NUM_BROWSERS = 2
-#fileReader = csv.reader(open('./sites/test.csv'), delimiter=',')
-fileReader = csv.reader(open('./sites/identified_detectors.csv'), delimiter=',')
+NUM_BROWSERS = 4
+fileReader = csv.reader(open('./sites/test_set.csv'), delimiter=',')
 sites = []
 for (index, site) in fileReader:
     sites.append(site);
@@ -23,8 +22,8 @@ manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
 for i in range(NUM_BROWSERS):
     browser_params[i]['headless'] = False  # Launch only browser 0 headless
 
-manager_params['data_directory'] = '~/Desktop/OpenWPM/data/'
-manager_params['log_directory'] = '~/Desktop/OpenWPM/data/'
+manager_params['data_directory'] = './Results/'
+manager_params['log_directory'] = './Results/'
 manager = TaskManager.TaskManager(manager_params, browser_params)    
 
 for i, site in enumerate(sites):
