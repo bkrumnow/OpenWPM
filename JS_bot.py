e75b4d593a44b8e76015e115041d3e0146c7ab8b
bkrumnow <benjamin.krumnow@th-koeln.de>
2019-07-23 11:11:29 +0200

    testing with leveldb

--- JS_bot.py
+++ JS_bot.py
@@ -22,7 +22,9 @@ manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
 for i in range(NUM_BROWSERS):
     browser_params[i]['headless'] = False  # Launch only browser 0 headless
     browser_params[i]['stealth_enabled'] = False # Avoid to load stealh extension
-    browser_params[i]['save_javascript'] = True # Avoid to load stealh extension
+    browser_params[i]['js_instrument'] = True
+    browser_params[i]['save_all_content'] = True
+    #browser_params[i]['save_javascript'] = True
         
 manager_params['data_directory'] = './Results/'
 manager_params['log_directory'] = './Results/'
@@ -31,7 +33,8 @@ manager = TaskManager.TaskManager(manager_params, browser_params)
 for i, site in enumerate(sites):
     command_sequence = CommandSequence.CommandSequence("http://" + site, True)
     command_sequence.get(sleep=20, timeout=60)
-    command_sequence.screenshot_full_page(site, 5)
+    command_sequence.save_screenshot(site, 5)
+    command_sequence.dump_page_source("source", 10)
     manager.execute_command_sequence(command_sequence, index=None)
     #del command_sequence
 manager.close()
